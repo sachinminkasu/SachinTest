126a70b7f4b087e44fc3c06faee58eaa6477f388
sachinminkasu <sachin@minkasu.com>
2018-10-15 16:37:31 -0700

    Added log line

--- SachinTest.framework/Headers/SachinTest.h
+++ SachinTest.framework/Headers/SachinTest.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char SachinTestVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SachinTest/PublicHeader.h>
 
+#import <SachinTest/Test.h>
+
 
