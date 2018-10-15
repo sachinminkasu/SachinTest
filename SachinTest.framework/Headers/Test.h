15993d7fa60dd488315ba002e534800109acab6c
sachinminkasu <sachin@minkasu.com>
2018-10-15 16:50:13 -0700

    Added test to public header

---
+++ SachinTest.framework/Headers/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  SachinTest
+//
+//  Created by Sachin Selvaraj on 10/15/18.
+//  Copyright © 2018 sachin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
++(void)printLog:(NSString *)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
