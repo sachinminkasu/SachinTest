ac6651bbb28a8d942d7deb19a7b9ae7606620fb4
sachinminkasu <sachin@minkasu.com>
2018-10-16 15:29:00 -0700

    Add files via upload

---
+++ SachinTest.framework/Headers/SachinLog.h
@@ -0,0 +1,19 @@
+//
+//  SachinLog.h
+//  SachinTest
+//
+//  Created by Sachin Selvaraj on 10/16/18.
+//  Copyright © 2018 sachin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SachinLog : NSObject
+
++(void)log:(NSString *)logString;
+
+@end
+
+NS_ASSUME_NONNULL_END
